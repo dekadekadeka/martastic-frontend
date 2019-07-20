84720ba9371f30b8b0aca46dcb10e9a5f8cd9911
github username <dekadekadeka@gmail.com>
2019-07-20 09:06:41 -0400

    navbar styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Route, Switch, Redirect } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
-function App() {
+import Home from './pages/Home'
+import Users from './pages/Users'
+import Stations from './pages/Stations'
+import SingleStation from './pages/SingleStation'
+import Pics from './pages/Pics'
+import Login from './pages/Login'
+import Error from './pages/Error'
+import Profile from './pages/Profile'
+import About from './pages/About'
+
+import "./App.css"
+
+export default class App extends Component {
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/users/" component={Users} />
+        <Route exact path="/stations/" component={Stations} />
+        <Route exact path="/stations/:slug" component={SingleStation} />
+        <Route exact path="/pics/" component={Pics} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/about" component={About} />
+        {/* <Route exact path="/profile"
+        render={() => (loggedIn ? <Profile /> : <Redirect to="/login" />)} /> */}
+        <Route component={Error} />
+        </Switch>
     </div>
-  );
+    )
+  }
 }
-
-export default App;
