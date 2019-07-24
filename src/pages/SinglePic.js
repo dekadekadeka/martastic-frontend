8ab13fcb7269352331154f598a8c9af5c0b835d9
github username <dekadekadeka@gmail.com>
2019-07-23 22:00:03 -0400

    single pic comments

--- src/pages/SinglePic.js
+++ src/pages/SinglePic.js
@@ -9,11 +9,42 @@ class SinglePic extends Component {
 
     render() {
         if(this.props.selected === undefined) return null
-        console.log(this.props.selected)
+        console.log(this.props.selected.comments)
+//         const allComments = this.props.selected.comments.map(comment => (
+//             <div className="comment">
+//     <a className="avatar">
+//       <img src={{...comment.user}.profile_pic_url}/>
+//     </a>
+//     <div className="content">
+//       <a className="author">{{...comment.user}.name}</a>
+//       <div className="metadata">
+//         <span className="date">{{...comment.user}.location}</span>
+//       </div>
+//       <div className="text">
+//         {comment.content}
+//       </div>
+//     </div>
+//   </div>
+//         ))
+
         return (
             <div className="single-pic">
-                Single Pic
-            </div>
+                <img className="big-pic" src={this.props.selected.pic_url} alt=""/>
+            
+            <div className="ui comments">
+            <h3 className="ui dividing header">Comments</h3>
+            {/* {allComments} */}
+  
+  <form className="ui reply form">
+                <div className="field">
+                <textarea></textarea>
+                </div>
+                <div className="ui blue labeled submit icon button">
+                <i className="icon edit"></i> Add Reply
+                </div>
+            </form>
+    </div>
+  </div>
         )
     }
 }
