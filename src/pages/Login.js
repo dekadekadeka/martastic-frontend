be158aa90ad62c0a526aed5d8ee8a30e1adfb297
github username <dekadekadeka@gmail.com>
2019-07-28 12:06:54 -0400

    redirect when new  user is created

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -54,7 +54,7 @@ class Login extends Component {
     )
   }
 }
-
+//yay!
 const mapDispatchToProps = dispatch => ({
   userLoginFetch: (userInfo, history) => dispatch(userLoginFetch(userInfo, history))
 });
