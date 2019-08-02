6160b64002bf051450266b4224e8101f698142a8
github username <dekadekadeka@gmail.com>
2019-08-02 09:24:05 -0400

    backup

--- src/store.js
+++ src/store.js
@@ -1,8 +1,20 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers'
+///BELOW IS JUST A BACKUP!!! in case the real Marta API goes down///
+//please uncomment below//
+//and comment out the blank initialState//
 
-const initialState = {};
+// import backup from '../src/backup-api.json'
+
+// const initialState = {
+//     schedule: {
+//         trains: backup,
+//         sortedTrains: backup
+//     }
+// };
+
+const initialState = {}
 
 const middleware = [thunk];
 
