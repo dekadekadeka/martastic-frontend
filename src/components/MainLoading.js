efc3de8572ab3909cc7356420c76d6ef183a1f02
github username <dekadekadeka@gmail.com>
2019-09-30 14:10:28 -0400

    loading text

---
+++ src/components/MainLoading.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class MainLoading extends Component {
+    render() {
+        return (
+            <div className="loading-text">
+                <h3>we are</h3>
+                <h1>Martastic</h1>
+            </div>
+        )
+    }
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,20 +2,22 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getRandomPic } from '../reducers/picReducer';
 import HomeDiv from '../components/HomeDiv';
+import MainLoading from '../components/MainLoading'
 
 
 class Home extends Component {
 
 
     render(){
-        if(this.props.pic === undefined) return null
+        if(this.props.pic === undefined) return <MainLoading />
         return (
-            <HomeDiv img={this.props.pic.pic_url}>
-                <div className="pic-info">
-                    <h1>{{...this.props.pic.station}.name}</h1>
-                    <h3>Photo by {{...this.props.pic.user}.name}</h3>
-                </div>
-            </HomeDiv>
+            // <HomeDiv img={this.props.pic.pic_url}>
+            //     <div className="pic-info">
+            //         <h1>{{...this.props.pic.station}.name}</h1>
+            //         <h3>Photo by {{...this.props.pic.user}.name}</h3>
+            //     </div>
+            // </HomeDiv>
+            <MainLoading/>
         )
     }
 
