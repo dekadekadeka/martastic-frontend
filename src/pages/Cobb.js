fb735824fbd72e311dcde637c1ccb92e165af005
Deka <dekadekadeka@gmail.com>
2020-02-29 22:43:40 -0500

    about page stuff

---
+++ src/pages/Cobb.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Cobb() {
+  console.log('  ___            _       _               __  __             _    ')
+  console.log(' / __|    ___   | |__   | |__      o O O|  \\/  |   ___     | |   ')
+  console.log('| (__    / _ \\  | \'_ \\  | \'_ \\    o     | |\\/| |  / -_)    |_|   ')
+  console.log(' \\___|   \\___/  |_.__/  |_.__/   TS__[O]|_|__|_|  \\___|   _(_)_  ')
+  console.log('_|"""""|_|"""""|_|"""""|_|"""""| {======|_|"""""|_|"""""|_| """ | ')
+  console.log('"`-0-0-\'"`-0-0-\'"`-0-0-\'"`-0-0-\'./o--000\'"`-0-0-\'"`-0-0-\'"`-0-0-\'')
+  console.log('But if you\'re smart enough to have found this... 🥰')
+  console.log('🍆🍑💦🥰')
+  console.log('We can talk about it 😘')
+  return (
+    <div className="log-in">
+      <h1 style={{ fontSize: 'xxx-large' }}>I am ashamed of you.</h1>
+      <h2>Did you really expect that from me? This is a professional place.</h2>
+      <h2>If you're looking for that other thing, there's the whole internet for that.</h2>
+      <h2>I am honestly insulted that you came all the way here to look for this.</h2>
+      <h2>Go back and look at the train schedule or something.</h2>
+      <h1>Much love,</h1>
+      <h1>Deka</h1>
+    </div>
+  )
+}
