cac25fcff9a9cd4fb963338cd9c32d9ed23999c7
Deka Ambia <dekadekadeka@gmail.com>
2021-08-06 00:56:56 -0400

    remove userReducer/Actions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,13 +4,11 @@ import picReducer from './picReducer';
 import authReducer from './authReducer';
 import stationReducer from './stationReducer';
 import commentReducer from './commentReducer'
-// import userReducer from './userReducer'
 
 export default combineReducers({
-    schedule: scheduleReducer,
-    pics: picReducer,
-    currentUser: authReducer,
-    stations: stationReducer,
-    comment: commentReducer,
-    // user: userReducer
+  schedule: scheduleReducer,
+  pics: picReducer,
+  currentUser: authReducer,
+  stations: stationReducer,
+  comment: commentReducer,
 })
